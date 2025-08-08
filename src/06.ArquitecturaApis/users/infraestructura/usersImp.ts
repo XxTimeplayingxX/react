dbd5a131972d967f1cbbab162fa34a47701d339b
David <david.sanchez@tecnologicoliceocristiano.edu.ec>
2025-08-08 09:09:44 -0500

    restantes

---
+++ src/06.ArquitecturaApis/users/infraestructura/usersImp.ts
@@ -0,0 +1,17 @@
+import type { UserRepository } from "../domain/repository/userRepository";
+import type { User } from "../user";
+
+const URL = 'https://jsonplaceholder.typicode.com/users';
+
+export class UserRepositoryImpClass implements UserRepository{
+    async getAll():Promise<User[]>{
+        const response = await fetch(URL);
+
+        if(!response.ok){
+            throw new Error('Error al cargar usuarios')
+        }
+        return await response.json;
+    }
+}
+
+export const UserRepository = new userRe
